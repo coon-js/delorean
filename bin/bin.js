e74321ae2d4944b4379f1985bf6109ccc4632801
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2022-11-21 14:13:01 +0100

    refactor: minor updates to sources and docs

--- bin/bin.js
+++ bin/bin.js
@@ -1,5 +1,31 @@
 #!/usr/bin/env node
 
+/**
+ * coon.js
+ * delorean
+ * Copyright (C) 2022 Thorsten Suckow-Homberg https://github.com/coon-js/delorean
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
 import fs from "fs-extra";
 import l8 from "@l8js/l8";
 import path from "path";
@@ -306,6 +332,9 @@ const PREPARE = () => {
 
 const CLEANUP = () => {
     changeProjectConfig(true);
+
+    log(chalk.green("If you stick a Babel fish in your ear you can instantly understand anything..."));
+    log(chalk.yellow("            <_>< "));
 };
 
 
